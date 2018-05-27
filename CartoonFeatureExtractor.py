a4db35833bb9ba960b2cbad69954f74562c231bc
Alex G <AresEyE@163.com>
2018-05-27 16:43:21 +0800

    little faster

--- CartoonFeatureExtractor.py
+++ CartoonFeatureExtractor.py
@@ -1,8 +1,10 @@
 #encoding:utf8
 import os, sys, re, pdb
 import codecs
+import time
 import numpy as np
 from PIL import Image
+import collections
 import colorsys
 from skimage.feature import hog, daisy
 import matplotlib.pyplot as plt
@@ -10,9 +12,9 @@ import matplotlib.pyplot as plt
 def MaxColorNumber(img, colorCountThreshold = 10,
         VThreshold = 125):
     # Fixed length
-    N = 125
+    N = 126
     vec = []
-    colors = img.getcolors(maxcolors = 256 ** 3)
+    # colors = img.getcolors(maxcolors = 256 ** 3)
     rgbArray = list(img.getdata())
 
     # RGB histogram
@@ -20,14 +22,27 @@ def MaxColorNumber(img, colorCountThreshold = 10,
     vec.extend(np.histogram(rArray, bins = 20)[0])
     vec.extend(np.histogram(gArray, bins = 20)[0])
     vec.extend(np.histogram(bArray, bins = 20)[0])
-    colorCountArray = [x[0] for x in colors]
-    colorCount = len(colors)
-    majorColorCount = len(filter(lambda x: x[0] > colorCountThreshold, colors))
-    top10Count = sum(sorted(colorCountArray, reverse = True)[:10])
-    Top10Ratio = int(top10Count * 1000 / sum(colorCountArray))
+
+    # Counter
+    majorColorSet = set()
+    # colorCounter = collections.Counter()
+    colorCounter = collections.defaultdict(int)
+    for rgb in rgbArray:
+        colorCounter[rgb] += 1
+        if colorCounter[rgb] > colorCountThreshold:
+            majorColorSet.add(rgb)
+    colorCounterArray = list(colorCounter.items())
+    colorCounterArray.sort(key = lambda x:x[1], reverse = True)
+    colorCount = len(colorCounter)
+    majorColorCount = len(majorColorSet)
+    top10CommonColor = colorCounterArray[:10]
+    top10Count = sum([x[1] for x in top10CommonColor])
+    Top10Ratio = int(top10Count * 1e6 / len(rgbArray))
+    majorColorRatio = int(sum([colorCounter[x] for x in majorColorSet]) * 1e6 / len(rgbArray))
 
     # HSV
-    hsvArray = [[x[0], colorsys.rgb_to_hsv(x[1][0], x[1][1], x[1][2])] for x in colors]
+    hsvArray = [[colorCounter[x],
+        colorsys.rgb_to_hsv(x[0], x[1], x[2])] for x in colorCounter]
     count = 0
     avgS = 0
     Vcount = 0
@@ -45,6 +60,7 @@ def MaxColorNumber(img, colorCountThreshold = 10,
     vec.extend(np.histogram([x[1][1] for x in hsvArray], bins = 20)[0])
     vec.extend(np.histogram([x[1][2] for x in hsvArray], bins = 20)[0])
 
+    vec.append(majorColorRatio)
     vec.append(colorCount)
     vec.append(majorColorCount)
     vec.append(Top10Ratio)
@@ -61,17 +77,17 @@ def GetImageFeatures(imagePath):
 
     # Append Features
     features = []
-    features.extend(MaxColorNumber(img))
-    features.extend(MorphFeatures(img))
-    
     # Color Features
+    features.extend(MaxColorNumber(img))
+
+    features.extend(MorphFeatures(img))
 
     # Close
     img.close()
     return features
 
 def MorphFeatures(img):
-    vecN = 32 + 32 * 2
+    vecN = 32 #+ 32 * 2
     vec = []
     greyArray = list(img.convert('L').getdata())
     M = img.height
@@ -80,25 +96,34 @@ def MorphFeatures(img):
     image = np.reshape(greyArray, [M, N])
     
     # HOG
-    res = hog(image, orientations=32, pixels_per_cell=(cellN, cellN),
-                    cells_per_block=(1, 1))
+    res = hog(image, orientations=32,
+            pixels_per_cell=(cellN, cellN),
+                    cells_per_block=(1, 1),
+                    block_norm = 'L2-Hys',
+                    feature_vector = True)
+    res = res.reshape((-1,32))
+    res = np.average(res, 0)
     vec.extend(res.tolist())
 
     # Daisy
-    res = daisy(image, radius = cellN / 2 - 1, histograms = 1)
-    daisyShape = res.shape
-    res = res.reshape(daisyShape[0] * daisyShape[1], daisyShape[2])
-    res *= 1e6
-    res = res.astype(int)
-    vec.extend(res[0,:].tolist())
-    vec.extend(res[-1,:].tolist())
-    assert len(vec) == vecN
+    # res = daisy(image, radius = cellN / 2 - 1, histograms = 1)
+    # daisyShape = res.shape
+    # res = res.reshape(daisyShape[0] * daisyShape[1], daisyShape[2])
+    # res *= 1e6
+    # res = res.astype(int)
+    # vec.extend(res[0,:].tolist())
+    # vec.extend(res[-1,:].tolist())
+    if len(vec) != vecN:
+        raise Exception('res.shape = {}'.format(res.shape))
     return vec
     
 def main():
     # imagePath = './data/cc/n1.jpg'
-    imagePath = './data/cc/F0F9D13586BDD65EC48AE36F313C3A16C71ACFF8.jpg'
+    # imagePath = './data/cc/F0F9D13586BDD65EC48AE36F313C3A16C71ACFF8.jpg'
+    imagePath = './data/part500/F08233C3426280CE2C8E6B4C711ACDFED87E443E.jpg'
+    start_time = time.time()
     vec = GetImageFeatures(imagePath)
+    print('{} s'.format(time.time() - start_time))
     print('Number of features:', len(vec))
 
 if __name__ == '__main__':
